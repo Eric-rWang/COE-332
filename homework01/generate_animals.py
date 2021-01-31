c184a05a24eb3be15760e01129d3f0bd4681663f
Eric Wang <ericrwang2011@gmail.com>
2021-01-31 15:34:45 -0600

    fixed rand_body()

--- homework01/generate_animals.py
+++ homework01/generate_animals.py
@@ -20,7 +20,7 @@ def rand_head():
 
 # returns random body type using petname library
 def rand_body():
-	return petname.generate(words=2, separator='-', letters=6)
+	return petname.name() + "-" + petname.name()
 
 # returns random number of arms
 def rand_arms():
