d7905ded0e1372c9937751bef52fa379e8eb2026
Eric Wang <ericrwang2011@gmail.com>
2021-04-23 14:52:52 -0500

    hw7

--- homework07/source/jobs.py
+++ homework07/source/jobs.py
@@ -4,6 +4,9 @@ import uuid
 from hotqueue import HotQueue
 from redis import StrictRedis
 
+q = HotQueue("queue", host='10.99.12.229', port=6379, db=1)
+rd = redis.StrictRedis(host='10.99.12.229', port=6379, db=0)
+
 def _generate_jid():
     return str(uuid.uuid4())
 
@@ -41,7 +44,15 @@ def add_job(start, end, status="submitted"):
     queue_job(jid)
     return job_dict
 
-
+def update_job_status(jid, new_status):
+    """Update the status of job with job id `jid` to status `status`."""
+    jid, status, start, end = rd.hmget(_generate_job_key(jid), 'id', 'status', 'start', 'end')
+    job = _instantiate_job(jid, status, start, end)
+    if job:
+        job['status'] = new_status
+        _save_job(_generate_job_key(job['id']), job)
+    else:
+        raise Exception()
 
 
 
