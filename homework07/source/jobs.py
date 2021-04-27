0c7df2f3ad623225f10e27da606b1367a091e798
Eric Wang <ericrwang2011@gmail.com>
2021-04-26 21:30:07 -0500

    worker change

--- homework07/source/jobs.py
+++ homework07/source/jobs.py
@@ -6,7 +6,8 @@ from hotqueue import HotQueue
 
 q = HotQueue("queue", host='10.99.12.229', port=6379, db=1)
 rd = redis.StrictRedis(host='10.99.12.229', port=6379, db=0)
-worker_ip = os.environ.get('WORKER_IP')
+#worker_ip = os.environ.get('WORKER_IP')
+#print(worker_ip)
 
 def _generate_jid():
     return str(uuid.uuid4())
@@ -37,7 +38,7 @@ def _queue_job(jid):
     """Add a job to the redis queue."""
     q.put(jid)
 
-def add_job(start, end, status="submitted"):
+def add_job(start, end, status = "submitted"):
     """Add a job to the redis queue."""
     jid = _generate_jid()
     job_dict = _instantiate_job(jid, status, start, end)
@@ -47,14 +48,14 @@ def add_job(start, end, status="submitted"):
     _queue_job(jid)
     return job_dict
 
-def update_job_status(jid, new_status):
+def update_job_status(jid, new_status, worker_ip = ""):
     """Update the status of job with job id `jid` to status `status`."""
-    jid, status, start, end, pod_ip = rd.hmget(_generate_job_key(jid), 'id', 'status', 'start', 'end', 'pod_ip')
+    jid, status, start, end = rd.hmget(_generate_job_key(jid), 'id', 'status', 'start', 'end')
     # if it is in progess, assign worker ip, else do not, just read it from database.
-    if new_status == "in progress":
-        pod_ip = worker_ip
+    if new_status == "complete":
+        pod_ip = rd.hmget(_generate_job_key(jid), 'pod_ip')
 
-    job = _instantiate_job(jid, status, start, end, pod_ip)
+    job = _instantiate_job(jid, status, start, end, worker_ip)
     if job:
         job['status'] = new_status
         _save_job(_generate_job_key(job['id']), job)
--- homework07/source/worker.py
+++ homework07/source/worker.py
@@ -1,10 +1,12 @@
 # worker.py
-import time
+import time, os
 from jobs import q, update_job_status
+
+worker_ip = os.environ.get('WORKER_IP')
 
 @q.worker
 def execute_job(jid):
-    update_job_status(jid, "in progress")
+    update_job_status(jid, "in progress", worker_ip)
     time.sleep(15)
     update_job_status(jid, "complete")
 
